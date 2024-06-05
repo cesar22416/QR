ec28275e329a067dc23485ca7d7f02cadb97c0be
Cesar Montilla <115080014+cesar22416@users.noreply.github.com>
2024-06-05 13:40:48 -0300

    version1.01

--- Generador de Qr/script.js
+++
@@ -1,9 +0,0 @@
-const contenedorQR = document.getElementById('contenedorQR');
-const formulario = document.getElementById('formulario');
-
-const QR = new QRCode(contenedorQR);
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-	QR.makeCode(formulario.link.value);
-});
